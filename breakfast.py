a57d3c2719a3d60222e2cf8fe8812ea6afc8bf99
suzan_k <kataikesuzan2@gmail.com>
2017-04-17 15:43:21 +0300

    introduction to git

---
+++ breakfast.py
@@ -0,0 +1,15 @@
+""""breakfast"""
+def serve_breakfast (meal):
+    print ("flip to the other side")
+    breakfast = "---Yummy %s ---" % meal
+    print (breakfast)
+    print ("Get the ingridients")
+    print ("Mix")
+    print ("Pour into the heated pan")
+    print ("Flip to the other side")
+    breakfast = "---Yummy pancake---"
+    return (breakfast)
+
+print(serve_breakfast("eggs"))
+    
+
